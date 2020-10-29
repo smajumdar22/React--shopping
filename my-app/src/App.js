3820310d934ba4946dd2b1b4c73806fca82ea303
smajumdar22 <smajumdar22@gmail.com>
2020-10-29 16:02:55 -0700

    Add basic template and styling

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+  <div className="grid-container">
+    <header>
+      <a href="/">My Shopping Cart</a>
+    </header>
+    <main>
+      Product List
+    </main>
+    <footer>
+      My website
+    </footer>
+  </div>
+  );
+}
+
+export default App;
